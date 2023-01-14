ece844aacc468fdfdb572d502b58e74565ea9c30
42PNk <konstantin.oreshkin.ism@gmail.com>
2023-01-14 18:22:27 +0500

    pages/base_page.py, pages/main_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
