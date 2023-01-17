680bd05b27912982fc2a0cecaa738386490c2c9a
42PNk <konstantin.oreshkin.ism@gmail.com>
2023-01-17 21:40:08 +0500

    Upload /pages/locators.py, /pages/login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert current_url == 'http://selenium1py.pythonanywhere.com/ru/accounts/login/', "Login URL is not presented"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Register form is not presented"
+       
