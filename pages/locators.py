c6d1086cc4bd5038deba2b6b0f5694f50921b5e9
42PNk <konstantin.oreshkin.ism@gmail.com>
2023-01-18 19:40:15 +0500

    Upload test_product_page.py, /pages/product_page.py, Edit /pages/base_page.py, /pages/locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,15 @@
 from selenium.webdriver.common.by import By
 
-
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProducttLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .locators import ProducttLocators
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+    def product_add_to_basket(self):
+        add_product = self.browser.find_element(*ProducttLocators.ADD_BASKET)
+        add_product.click()
+
+    def should_be_name_in_basket_equal_product(self):
+        product_name = self.browser.find_element(*ProducttLocators.PRODUCT_NAME).text
+        basket_name = self.browser.find_element(*ProducttLocators.BASKET_NAME).text
+        print (f"{basket_name} add in basket!")
+        assert product_name == basket_name, "Names are not equal"
+       
+
+    def should_be_price_in_basket_equal_product(self):
+        product_price = self.browser.find_element(*ProducttLocators.PRODUCT_PRICE).text
+        basket_price = self.browser.find_element(*ProducttLocators.BASKET_PRICE).text
+        print(f"Basket price is {basket_price}")
+        assert product_price == basket_price, "prices are not equal"
